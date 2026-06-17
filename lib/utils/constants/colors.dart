778d97bea89b7e159827cfe8102f87d8cee408b8
deema ashraf jaber <deemaashrafjaber@gmail.com>
2026-06-17 13:51:32 +0300

    Add SecondaryColor

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -4,6 +4,7 @@ class TColors {
   TColors._();
 
   static const Color PrimaryColor = Color(0xFF0B3C73);
+  static const Color SecondaryColor = Color(0xffE2E2E2);
   static const Color textColor = Color(0xFF2C3E50);
   static const Color backgroundColor = Color(0xFFF8FDFD);
 }
